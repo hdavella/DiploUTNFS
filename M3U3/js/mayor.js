e06968388c14995ba28a0dfcdaeb98b17ed4690e
hdavella <hdavella@gmail.com>
2022-11-04 20:01:34 -0300

    mayor

--- M3U3/js/mayor.js
+++ M3U3/js/mayor.js
@@ -1,1 +1,13 @@
-const cantidadaRecorrer = parseInt(prompt('Ingrese la cantidad de elemntos: ', ''));
+const cantidadElementos = parseInt(prompt('Ingrese la cantidad de elementos de tipo entero: ', ''));
+const elementoNumero = new Array(cantidadElementos);
+var mayor = 0;
+
+for (let i = 0; i < elementoNumero.length; i++) {
+
+    elementoNumero[i] = parseInt(prompt(`Ingrese el valor del elemento ${i + 1} : `, ''));
+    if (elementoNumero[i] >= mayor) {
+        mayor = elementoNumero[i];
+    }
+}
+
+console.log(`El número mayo de los ingresados es: ${mayor}`);
