33b8f0910c24222adead9eb211211d9d082ba33e
hdavella <hdavella@gmail.com>
2022-11-14 00:08:33 -0300

    ejercicio3

---
+++ M3U4/js/ejercicio3.js
@@ -0,0 +1,66 @@
+const alumnos = [
+
+    {
+
+        nombre: 'Juan Gomez',
+
+        nota: 7
+
+    }, {
+
+        nombre: 'Pedro Rodriguez',
+
+        nota: 4
+
+    }, {
+
+        nombre: 'Roxana García',
+
+        nota: 8
+
+    }, {
+
+        nombre: 'Luciano Lopez',
+
+        nota: 5
+
+    }, {
+
+        nombre: 'Fernanda Gimenez',
+
+        nota: 6
+
+    }, {
+
+        nombre: 'Florencia Martinez',
+
+        nota: 10
+
+    }, {
+
+        nombre: 'Raul Sanchez',
+
+        nota: 7
+
+    }, {
+
+        nombre: 'Sandra Figueroa',
+
+        nota: 8
+
+    }
+
+];
+function imprimir() {
+    /* const aprobados = alumnos.map(index => index.nota >= 7);
+    console.log(aprobados); */
+
+    const aprobados = alumnos.filter(index => index.nota >= 7);
+    console.log(aprobados);
+    
+    for (let index = 0; index < alumnos.length; index++) {
+        console.log(aprobados[index].nombre);
+    }
+    
+}
+
