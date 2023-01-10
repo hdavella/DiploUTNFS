e688ead0089106b16e12eba4d15013091db8e222
hdavella <hdavella@gmail.com>
2023-01-10 14:49:29 -0300

    comienzo login

--- Transporte-React/backend/app.js
+++ Transporte-React/backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 // generado por hdavella
 app.use('/admin/login', loginRouter);
 
