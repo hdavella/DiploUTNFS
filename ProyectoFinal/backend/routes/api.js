4cd14a61678a6e3fa7d9fbcee9c6cfcdcafb72b3
hdavella <hdavella@gmail.com>
2023-02-03 10:31:25 -0300

    solo falta el mail y la estetica del sitio

--- ProyectoFinal/backend/routes/api.js
+++ ProyectoFinal/backend/routes/api.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var trabajosModel = require('../models/trabajosModel');
+var cloudinary = require('cloudinary').v2;
 
 router.get('/trabajos', async (req, res, next) =>{
 
     let trabajos = await trabajosModel.getTrabajos();
+
+    trabajos = trabajos.map( (trabajo)=>{
+        if(trabajo.img_id){
+            const imagen = cloudinary.url(trabajo.img_id,{
+                width:100,
+                height:100,
+                crop:"fill"
+            });
+            return{
+                ...trabajo, imagen
+            }
+        }else{
+            return{
+                ...trabajo, imagen:""
+            }
+        }
+    });
+
+
     res.json(trabajos);
 });
 
--- ProyectoFinal/frontend/src/pages/TrabajosPage.js
+++ ProyectoFinal/frontend/src/pages/TrabajosPage.js
@@ -1,58 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import TrabajoItem from '../components/trabajos/TrabajoItem';
 import "../styles/trabajosPage.css";
 
+
 const TrabajosPage = (props) => {
-    return(
-        <main className="holder">
-      <h2>Servicios</h2>
-      <div className="servicio">
-        <img src="img/servicios/ferroviario.jpg" alt="Tren"/>
-        <div className="info">
-          <h4>Transporte Ferroviario</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Porro tempore facere sint eligendi aut officiis earum cum, 
-            hic numquam pariatur accusamus perspiciatis quis! Laborum 
-            esse tempora explicabo sit est sunt?
-          </p>
-        </div>
-      </div>
 
-      <div className="servicio">
-        <img src="img/servicios/aereo.jpg" alt="Tren"/>
-        <div className="info">
-          <h4>Transporte Aéreo</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Porro tempore facere sint eligendi aut officiis earum cum, 
-            hic numquam pariatur accusamus perspiciatis quis! Laborum 
-            esse tempora explicabo sit est sunt?
-          </p>
-        </div>
-      </div>
+  const [loading, setLoading] = useState(false);
+  const [trabajos, setTrabajos] = useState([]);
 
-      <div className="servicio">
-        <img src="img/servicios/maritimo.jpg" alt="Tren"/>
-        <div className="info">
-          <h4>Transporte Marítimo</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Porro tempore facere sint eligendi aut officiis earum cum, 
-            hic numquam pariatur accusamus perspiciatis quis! Laborum 
-            esse tempora explicabo sit est sunt?
-          </p>
-        </div>
-      </div>
+  useEffect( ()=>{
+    const cargarTrabajos = async ()=>{
+      setLoading(true);
+      const response = await axios.get('http://localhost:3000/api/trabajos');
+      setTrabajos(response.data);
+      setLoading(false);
+    };
+    cargarTrabajos();
+  }, []);
 
-      <div className="servicio">
-        <img src="img/servicios/terrestre.jpg" alt="Tren"/>
-        <div className="info">
-          <h4>Transporte Terrestre</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Porro tempore facere sint eligendi aut officiis earum cum, 
-            hic numquam pariatur accusamus perspiciatis quis! Laborum 
-            esse tempora explicabo sit est sunt?
-          </p>
-        </div>
-      </div>
-    </main>
+  return(
+    <section className='holder'>
+      <h2>Trabajos</h2>
+      {
+        loading ? (
+        <p>Cargando...</p>
+        ):(
+          trabajos.map(item => <TrabajoItem key={item.id} title={item.titulo} subtitle={item.subtitulo} imagen={item.imagen} body={item.descripcion} />
+            
+
+            )
+          )
+      }
+    </section> 
+
     );
 }
 export default TrabajosPage;
